2016155191eb01d5a716ae31d37130e1f8b059ed
kateMoscoso <katiemscs@gmail.com>
2020-06-24 17:50:36 +0200

    diagonal refactor

--- examples/diagonal.js
+++ examples/diagonal.js
@@ -1,15 +1,22 @@
 function diagonalDifference(arr) {
     // Write your code here
-    
     let left_to_right_diagonal_sum = 0
     let right_to_left_diagonal_sum = 0
+    let column = arr.length - 1;
+    let sum = 0
     for(let i = 0 ; i< arr.length;  i++){
-        left_to_right_diagonal_sum+=arr[i][i]
-        for(let j = 0 ; j< arr.length;  j++){
-            if((j+i) == arr.length-1){
-                right_to_left_diagonal_sum+=arr[i][j]
-            }
-        }
+        left_to_right_diagonal_sum += arr[i][i]
+        right_to_left_diagonal_sum += arr[i][column--]
     }
     return Math.abs(left_to_right_diagonal_sum - right_to_left_diagonal_sum)
+}
+
+function diagonalDifference2(arr) {
+    // Write your code here
+    let sum = 0
+    let column = arr.length - 1;
+    for(let i = 0 ; i< arr.length;  i++){
+        sum += arr[i][i] - arr[i][column--]
+    }
+    return Math.abs(sum)
 }
